67298dcdade0ffd902978bc4a06ed25e18f7a7ff
TanishaAdhikari <itschhaya19k@gmail.com>
2026-06-04 15:38:27 +0545

    Update userRoutes.js

--- userRoutes.js
+++ userRoutes.js
@@ -1,25 +1,96 @@
 import{ Router } from 'express';
+import User from '../schema/userSchema.js';
 
-const userRoutes = Router();
+let userRoutes = Router();
 
 userRoutes
 .route("/")
-.post((req,res,next)=>{
-res.json("Product created")//postman lai dina pare res lina pare req
+.post(async(req,res,next)=>{
+//postman lai dina pare res lina pare 
+/* send data from postman
+get data sent by postman
+store data in user table  */
+try {
+    let result = await User.create(req.body);
+    res.json({
+    success: true,
+    message: "User created successfully", //yesley  direct postman ma open huncha
+    result: result,
 })
-.get((req,res,next)=>{
-res.json("Products retrieved")
+} catch (error) {
+    res.json({
+        success: false,
+        error: error.message
+    });
+}
+let result = await User.create(req.body); //try catch garna parcha kina bhane data create garna garda error auna sakcha
+})
+.get(async(req,res,next)=>{
+
+    try {
+        let result = await User.find();
+        res.json({
+            success: true,
+            message: "Users found successfully",
+            result: result
+        });
+    } catch (error) {
+        res.json({
+            success: false,
+            error: error.message
+        }); 
+    }
+
 })
 
 userRoutes
 .route("/:id")
-.get((req,res,next)=>{
+.get(async(req,res,next)=>{
+    try {
+        let result = await User.findById(req.params.id);
+        res.json({
+            success: true,
+            message: "User found successfully",
+            result: result
+        });
+    } catch (error) {
+        res.json({
+            success: false,
+            error: error.message
+        });
+    }
+    // console.log(req.params.id);
 })
-.patch((req,res,next)=>{
+.patch(async(req,res,next)=>{
+ try {
+    let result = await User.findByIdAndUpdate(req.params.id,req.body,{new: true});
+    res.json({
+        success: true,
+        message: "User updated successfully",
+        result: result
+    });
+ } catch (error) {
+    res.json({
+        success: false,
+        error: error.message
+    });
+ }
+})
+.delete(async(req,res,next)=>{
+ try {
 
-})
-.delete((req,res,next)=>{
-
-})
+    let result =await User.findByIdAndDelete(req.params.id);
+    res.json({
+        success: true,
+        message: "User deleted successfully",
+        result: result
+    });
+} catch (error) {
+    res.json({
+        success: false,
+        error: error.message,
+    });
+}
+});
 
 export default userRoutes;
